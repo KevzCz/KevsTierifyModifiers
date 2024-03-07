c258537941c020b1fcc7e807250ae356a39fc68f
KevinCz <lenguyenhoangthanh123@gmail.com>
2024-03-07 19:52:52 +0700

    changed description

---
+++ src/main/java/net/kevstierifymodifiers/KevsTierifyModifiers.java
@@ -0,0 +1,23 @@
+package net.kevstierifymodifiers;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.resources.ResourceLocation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class KevsTierifyModifiers implements ModInitializer {
+    public static String MOD_ID = "kevstierifymodifiers";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Hello from " + MOD_ID + "!");
+        LOGGER.info("Loading tiered data pack for " + MOD_ID);
+        FabricLoader.getInstance().getModContainer(MOD_ID).ifPresent((modContainer -> ResourceManagerHelper.registerBuiltinResourcePack(
+                new ResourceLocation(MOD_ID, "tierifycompat"), modContainer, ResourcePackActivationType.ALWAYS_ENABLED
+        )));
+    }
+}
